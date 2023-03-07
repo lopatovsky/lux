aefadca2a8daddcf4e1b2ff25bbe143988b01cc5
jamaisvu <lopatovsky@gmail.com>
2023-03-07 16:17:28 +0700

    add manualy tuned actions

--- agent.py
+++ agent.py
@@ -30,8 +30,11 @@ class Agent:
         # TODO why ObsWrapper is not init here
         self.controller = LuxController(self.state)
 
+        self.units = dict()
+        self.factories = dict()
+
     def bid_policy(self):
-        return dict(faction="AlphaStrike", bid=0)
+        return dict(faction="AlphaStrike", bid=1)
 
     def factory_placement_policy(self):
         """
@@ -53,6 +56,10 @@ class Agent:
         # Find location that is adjacent to ice with minimal rubble available
         for spawn_loc in potential_spawns:
             x, y = spawn_loc
+
+            closest_ice = self.state.ice_distance[x,y]
+            ice_dist = abs(closest_ice[0]) + abs(closest_ice[1])
+
             min_rubble = 100
             count_zeros = 0
             has_ice = False
@@ -67,6 +74,10 @@ class Agent:
                         if state.rubble[i, j] == 0:
                             count_zeros += 1
             min_rubble -= count_zeros  # count_zeros is nonzero only if min_rubble is 0.
+
+            if ice_dist == 2 and min_rubble < 0:
+                min_rubble -= 5  # bonus for short path to ice TODO temporary need
+
             if has_ice and min_rubble < lowest_rubble:
                 lowest_rubble = min_rubble
                 pos = spawn_loc
@@ -77,37 +88,160 @@ class Agent:
 
     def act(self):
 
-        obs = LuxObservationWrapper.convert_obs(self.state)
+        # obs = LuxObservationWrapper.convert_obs(self.state)
+        #
+        # obs = th.from_numpy(obs).float()
+        # with th.no_grad():
+        #
+        #     # to improve performance, we have a rule based action mask generator for the controller used
+        #     # which will force the agent to generate actions that are valid only.
+        #     action_mask = (
+        #         th.from_numpy(self.controller.action_masks())
+        #         .unsqueeze(0)
+        #         .bool()
+        #     )
+        #
+        #     # SB3 doesn't support invalid action masking. So we do it ourselves here
+        #     features = self.ppo_model.policy.features_extractor(obs.unsqueeze(0))
+        #     x = self.ppo_model.policy.mlp_extractor.shared_net(features)
+        #     logits = self.ppo_model.policy.action_net(x)  # shape (1, N) where N=12 for the default controller
+        #
+        #     logits[~action_mask] = -1e8  # mask out invalid actions
+        #     dist = th.distributions.Categorical(logits=logits)
+        #     actions = dist.sample().cpu().numpy()  # shape (1, 1)
+        #
+        # # use our controller which we trained with in train.py to generate a Lux S2 compatible action
+        # lux_action = self.controller.action_to_lux_action(actions[0])
+        #
+        #
+        # # grows lichen
+        # for unit_id in self.state.factories.keys():
+        #     factory = self.state.factories[unit_id]
+        #     if 1000 - self.state.step < 201 and factory["cargo"]["water"] > 200:
+        #         lux_action[unit_id] = 2  # water and grow lichen at the very end of the game
+        #
+        # return lux_action
 
-        obs = th.from_numpy(obs).float()
-        with th.no_grad():
+        return self.rule_based_actions()
 
-            # to improve performance, we have a rule based action mask generator for the controller used
-            # which will force the agent to generate actions that are valid only.
-            action_mask = (
-                th.from_numpy(self.controller.action_masks())
-                .unsqueeze(0)
-                .bool()
-            )
+    def move_dist(self, x, y):
+        """Move directions: [0, 0], [0, -1], [1, 0], [0, 1], [-1, 0]"""
+        actions = []
+        if x > 0:
+            actions.append(np.array([0, 4, 0, 0, 0, x]))
+        elif x < 0:
+            actions.append(np.array([0, 2, 0, 0, 0, -x]))
+        if y > 0:
+            actions.append(np.array([0, 1, 0, 0, 0, y]))
+        elif y < 0:
+            actions.append(np.array([0, 3, 0, 0, 0, -y]))
+        return actions
 
-            # SB3 doesn't support invalid action masking. So we do it ourselves here
-            features = self.ppo_model.policy.features_extractor(obs.unsqueeze(0))
-            x = self.ppo_model.policy.mlp_extractor.shared_net(features)
-            logits = self.ppo_model.policy.action_net(x)  # shape (1, N) where N=12 for the default controller
+    def dig_action(self, n):
+        return [np.array([3, 0, 0, 0, 0, n])]
 
-            logits[~action_mask] = -1e8  # mask out invalid actions
-            dist = th.distributions.Categorical(logits=logits)
-            actions = dist.sample().cpu().numpy()  # shape (1, 1)
+    def recharge_action(self, till_capacity):
+        return [np.array([5, 0, 0, till_capacity, 0, 1])]
 
-        # use our controller which we trained with in train.py to generate a Lux S2 compatible action
-        lux_action = self.controller.action_to_lux_action(actions[0])
+    def pick_up_action(self, resource_code, pick_up_amount ):
+        return [np.array([2, 0, resource_code, pick_up_amount, 0, 1])]
+
+    def transfer_action(self, resource_code):
+        """material = (0 = ice, 1 = ore, 2 = water, 3 = metal, 4 power)"""
+        transfer_dir = 0
+        return [np.array([1, transfer_dir, resource_code, self.state.cfg.max_transfer_amount, 0, 1])]
+
+    def mine_resource_action(self, unit, resource_dist_map, resource_code):
+        actions = []
+
+        dist = resource_dist_map[unit.pos[0],unit.pos[1]]
+        home_pos = unit.init_pos
+
+        actions.extend(self.move_dist(dist[0], dist[1]))
+        actions.extend(self.dig_action(5))
+        actions.extend(self.move_dist(home_pos[0] - (unit.pos[0] + dist[0]), home_pos[1] - (unit.pos[1] + dist[1])))
+        actions.extend(self.transfer_action(resource_code))
+
+        # TODO check current power
+        power = 50
+        if unit.unit_type == "HEAVY":
+            power = 400
+
+        actions.extend(self.pick_up_action(4, power)) # 4: power
+
+        return actions
+    def mine_ice_action(self, unit):
+        return self.mine_resource_action(unit, self.state.ice_distance, 0)  # 0: ice
+
+        # find closest ice
+        # if home: charge till possible to do action -how to
+        # get_move_actions to get there and energy?
+        # dig -- how many times? + e
+        # get home + e
+        # if not enough energy:
+        # go home to charge first. # need action or just stay
+
+    def mine_ore_action(self, unit):
+        return self.mine_resource_action(unit, self.state.ore_distance, 1)
+        #rand_num = np.random.randint(low=1, high=5)
+
+    def remove_rubble_action(self, unit):
+        return self.mine_resource_action(unit, self.state.rubble_distance, 1)
+    # actions for robots:
+    # - dig ice
+    # - dig metal
+    # - dig rubbles
+    # - dig lichen of opponent
+    # special -> avoid crashes or kill opponent
+
+    # actions for factories:
+    # - create light robot
+    # - create heavy robot
+    # - water lichen
+
+    def remove_rubble(self, unit):
+        pass
+        # basically almost mining
+
+    # TODO move this to controller?
+    def rule_based_actions(self):
+        lux_action = dict()
+
+        units = self.state.units
+        factories = self.state.factories
+
+        for unit_id in units.keys():
+            unit = units[unit_id]
+
+            # if collision_risk(unit):
 
 
-        # grows lichen
-        for unit_id in self.state.factories.keys():
-            factory = self.state.factories[unit_id]
-            if 1000 - self.state.step < 201 and factory["cargo"]["water"] > 200:
-                lux_action[unit_id] = 2  # water and grow lichen at the very end of the game
+            if len(unit.action_queue) == 0:
+                if unit.unit_type == 'HEAVY':
+                    lux_action[unit_id] = self.mine_ice_action(unit)
+                else:
+                    lux_action[unit_id] = self.remove_rubble_action(unit)
+
+        # FACTORIES ACTION
+
+        # Create a heavy unit.
+        if len(units) == 0:
+            for factory_id in factories.keys():
+                lux_action[factory_id] = 1
+
+        # Create a light unit
+        else:
+            for factory_id in factories.keys():
+                factory = factories[factory_id]
+                if factory.cargo["metal"] < 45:
+                    continue
+                # todo don't create if there is something standing
+                lux_action[factory_id] = 0
+
+        for factory_id in self.state.factories.keys():
+            factory = self.state.factories[factory_id]
+            if factory.cargo["water"] > 6*(1000 - self.state.step):
+                lux_action[factory_id] = 2  # water and grow lichen at the very end of the game
 
         return lux_action
 
--- game_state.py
+++ game_state.py
@@ -1,8 +1,73 @@
 import sys
 import copy
+import queue
 import numpy as np
 
 from lux.config import EnvConfig
+
+class Unit:
+    def __init__(self, obs):
+        self.update(obs)
+
+        # mother factory location
+        self.init_pos = self.pos
+
+    def update(self, obs):
+        self.unit_id = obs["unit_id"]
+        self.power = obs["power"]
+        self.unit_type = obs["unit_type"]
+        self.pos = obs["pos"]
+        self.cargo = obs["cargo"]
+        self.action_queue = obs["action_queue"]
+
+class Factory:
+    def __init__(self, obs):
+        self.update(obs)
+
+    def update(self, obs):
+        self.unit_id = obs["unit_id"]
+        self.strain_id = obs["strain_id"]
+        self.power = obs["power"]
+        self.pos = obs["pos"]
+        self.cargo = obs["cargo"]
+
+def valid_loc(loc,x,y):
+    return ( loc[0]+x >= 0 and loc[0]+x < 48 and loc[1]+y >= 0 and loc[1]+y < 48 )
+
+def q_move(loc, dist, x, y):
+    return ( np.array([loc[0]+x,loc[1]+y]), np.array([dist[0]+x, dist[1]+y]) )
+
+# TODO this could be a c++ method
+def build_distance_map(map, object_marker):
+    EMPTY = -100
+
+    dist_map = np.full((48,48,2), EMPTY, dtype=int)
+
+    object_locations = np.argwhere(map >= object_marker)
+    #print(object_locations, file=sys.stderr)
+
+    q = queue.Queue()
+
+    for loc in object_locations:
+        q.put((loc, np.array([0,0])))
+
+    while not q.empty():
+        loc, dist = q.get()
+        # If the distance is already calculated.
+        if dist_map[loc[0],loc[1]][0] != EMPTY:
+            continue
+
+        dist_map[loc[0],loc[1]] = dist
+
+        for x,y in [[0, -1], [1, 0], [0, 1], [-1, 0]]:
+            if valid_loc(loc,x,y):
+                q.put( q_move(loc,dist,x,y) )
+
+    #np.set_printoptions(threshold=np.inf)
+    #print(map, file=sys.stderr)
+    #print(dist_map, file=sys.stderr)
+
+    return dist_map
 
 class GameState:
 
@@ -17,6 +82,11 @@ class GameState:
 
         self.ice = np.array(obs["obs"]["board"]["ice"])
         self.ore = np.array(obs["obs"]["board"]["ore"])
+
+        self.ice_distance = build_distance_map(self.ice, 1)
+        self.ore_distance = build_distance_map(self.ore, 1)
+        # TODO for rubble,oponent lichen.. but this changes~~
+
         self.factories_per_team = obs["obs"]["board"]["factories_per_team"]
 
         # Initial set of variables. Later they come as a diff
@@ -24,10 +94,16 @@ class GameState:
         self.rubble = np.array( self.board["rubble"])
         self.lichen = np.array( self.board["lichen"])
         self.lichen_strains = np.array( self.board["lichen_strains"])
+
+        self.rubble_distance = build_distance_map(self.rubble, 1)
+
         self.valid_spawns_mask = np.array(self.board["valid_spawns_mask"])
 
         self.previous_state = None
         self.set_variable_obs(obs)
+
+        self.units = dict()
+        self.factories = dict()
 
     def update(self, obs):
         self.previous_state = copy.copy(self)
@@ -51,10 +127,23 @@ class GameState:
         # from -7 to 1000
         self.step = obs["obs"]["real_env_steps"]
 
-        self.units = obs["obs"]["units"][self.me]
-        self.his_units = obs["obs"]["units"][self.him]
+        units_data = obs["obs"]["units"][self.me]
 
-        self.factories = obs["obs"]["factories"][self.me]
+        for unit_id in units_data.keys():
+            if unit_id in self.units:
+                self.units[unit_id].update(units_data[unit_id])
+            else:
+                self.units[unit_id] = Unit(units_data[unit_id])
+
+        factories_data = obs["obs"]["factories"][self.me]
+        for factory_id in factories_data.keys():
+            if factory_id in self.factories:
+                self.factories[factory_id].update(factories_data[factory_id])
+                #print("new_unit", factories_data[factory_id], file=sys.stderr)
+            else:
+                self.factories[factory_id] = Factory(factories_data[factory_id])
+
+        self.his_units = obs["obs"]["units"][self.him]
         self.his_factories = obs["obs"]["factories"][self.him]
 
         if  len(obs["obs"]["teams"]) > 0:
@@ -72,6 +161,8 @@ class GameState:
             self.update_board( self.rubble, self.board["rubble"])
             self.update_board( self.lichen, self.board["lichen"])
             self.update_board( self.lichen_strains, self.board["lichen_strains"])
+
+            #self.rubble_distance = build_distance_map(self.rubble, 1)
 
             if "valid_spawns_mask" in self.board:
                 #print(self.board["valid_spawns_mask"], file=sys.stderr)
